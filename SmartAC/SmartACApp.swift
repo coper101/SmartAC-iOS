9309fb7d5a10043252a4f5d982177dfaaa9e9d68
Daryl <darylgialolo@gmail.com>
2022-06-21 11:21:05 +0800

    Initial Commit

---
+++ SmartAC/SmartACApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartACApp.swift
+//  SmartAC
+//
+//  Created by Wind Versi on 21/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SmartACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
